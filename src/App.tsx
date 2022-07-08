cba44a98b289deaa53d98fccb9bb3d205fb76fa6
Alex Junior <alexalexandrejr@gmail.com>
2022-07-08 14:26:20 -0300

    chore: hello world

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ alignItems: 'center' }}>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
