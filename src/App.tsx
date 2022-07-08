c094290617495a42975da6d2779583fa9737ceab
Alex Junior <alexalexandrejr@gmail.com>
2022-07-08 15:12:34 -0300

    feat: adding splash screen on ios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 import { SafeAreaView, Text } from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <SafeAreaView style={{ alignItems: 'center' }}>
       <Text>Hello World</Text>
