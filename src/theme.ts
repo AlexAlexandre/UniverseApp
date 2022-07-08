b31b89acc37bac9d58ef91e06b76bf7e5aded8a3
Alex Junior <alexalexandrejr@gmail.com>
2022-07-08 18:00:19 -0300

    feat: adding theme file

---
+++ src/theme.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+
+export const colors = {
+  white: '#FFFFFF',
+};
+
+export const textStyles = StyleSheet.create({
+  bigTitle: {
+    fontSize: 60,
+  },
+  h1: {
+    fontSize: 32,
+  },
+  h2: {
+    fontSize: 26,
+  },
+  h3: {
+    fontSize: 18,
+  },
+  title: {
+    fontSize: 16,
+  },
+  body: {
+    fontSize: 12,
+  },
+  caption: {
+    fontSize: 14,
+  },
+  small: {
+    fontSize: 8,
+  },
+  // variations
+  bold: {
+    fontWeight: 'bold',
+  },
+  semiBold: {
+    fontWeight: '500',
+  },
+  light: {
+    fontWeight: '200',
+  },
+});
